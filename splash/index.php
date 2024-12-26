5872db2a596cdfd0ca7724c53bdb9969baa07893
Israel <lcc.isra@gmail.com>
2024-12-26 16:18:00 -0600

    splash

---
+++ splash/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+<head profile="http://gmpg.org/xfn/11">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="/assets/img/favicon.ico">
+  <link rel="shortcut icon" href="/assets/img/favicon.ico" type="image/vnd.microsoft.icon" /> 
+  <title>Tessin</title>
+  <meta property="og:title" content="Tessin" />
+  <meta property="og:type"  content="article" />
+  <meta property="og:url"   content="https://tessin.com.mx/" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css"/>
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css"/>
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.css" media="screen" /> 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="/assets/css/animate.css">
+  <link rel="stylesheet" type="text/css" href="/assets/css/main.css">  
+</head>
+	
+<body>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <span style="font-weight:700;font-size:50px;color:white;line-height:80%;">LOGO</span>
+        </div>
+        <div class="col-md-6">
+          <ul class="navigation">
+            <li></i><a href="tel:+19563601460" class="call"><i class="bi bi-phone"></i>+1 (956) 360-1460</a></li>
+            <li><a href="mailto:" class="btn">Contact Us <i class="bi bi-arrow-right-short"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>  
+
+  <section>
+    <div class="herobanner">
+      <div class="img-cover">
+        <img src="assets/img/bodega-industrial-monterrey-tessin.jpg">
+        <div class="caption">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <h1>Coming Soon</h1>
+                <p>Our website is under construction, but we are ready to go!</p>
+              </div>
+            </div>
+          </div>  
+        </div>
+      </div>
+      <div class="img-cover">
+        <img src="assets/img/construccin-monterrey-tessin.jpg">
+        <div class="caption">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <h1>Coming Soon</h1>
+                <p>Our website is under construction, but we are ready to go!</p>
+              </div>
+            </div>
+          </div>  
+        </div>
+      </div>
+      <div class="img-cover">
+        <img src="assets/img/bodegas-industriales-apodaca-pesqueria.jpg">
+        <div class="caption">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <h1>Coming Soon</h1>
+                <p>Our website is under construction, but we are ready to go!</p>
+              </div>
+            </div>
+          </div>  
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p>© Copyrights 2025 <span>TESSIN CAPITAL</span> - All Rights Reserved</p>
+          <a href="https://wa.me/19563601460" target="_blank" class="whatsapp icon-whatsapp"><i class="bi bi-whatsapp"></i></a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <script defer type="text/javascript" src='https://code.jquery.com/jquery-1.8.2.min.js'></script>
+  <script defer type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script> 
+  <script defer type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.js"></script>
+  <script defer type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/helpers/jquery.fancybox-media.js"></script>
+  <script defer type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.js"></script> 
+  <script defer type="text/javascript" src="/assets/js/main.js"></script>
+</body>
+</html>  
